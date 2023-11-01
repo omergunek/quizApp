7a5ea815f249f1c86f51bc82938cfb984042fc75
omer <gunekfaruk@hotmail.com>
2023-11-01 13:14:38 +0300

    Initial Commit

---
+++ quizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizApp
+//
+//  Created by Ömer Günek on 1.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
